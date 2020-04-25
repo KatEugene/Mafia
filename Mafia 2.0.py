fcb3c09952d7a2d219d2c9a2adce166e4642b211
KatEugene <katasonoveugene@yandex.ru>
2020-04-25 22:26:49 +0300

    Roles are described

--- Mafia 2.0.py
+++ Mafia 2.0.py
@@ -11,15 +11,25 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.alive = True
+        self.immunity = False
+
+    def __str__(self):
+        return f"Игрок: {self.name}, Жив: {self.alive}"
 
     def killed(self):
         self.alive = False
 
-    def alive(self):
+    def get_alive(self):
         return self.alive
 
-    def name(self):
+    def get_name(self):
         return self.name
+
+    def get_immunity(self):
+        return self.immunity
+
+    def set_immunity(self):
+        self.immunity = True
 
 
 class Civilian(Player):
@@ -33,24 +43,57 @@ class Mafia(Player):
 
     @staticmethod
     def kill(player):
-        player.killed()
+        if not player.immunity():
+            player.killed()
 
 
 class DonMafia(Player):
-    pass
+    def __init__(self, name):
+        super().__init__(name)
 
 
 class Maniac(Player):
-    pass
+    def __init__(self, name):
+        super().__init__(name)
+
+    @staticmethod
+    def kill(player):
+        if not player.immunity():
+            player.killed()
 
 
 class Detective(Player):
-    pass
+    def __init__(self, name):
+        super().__init__(name)
+
+    @staticmethod
+    def check(player):
+        if type(player) == DonMafia:
+            return "DonMafia"
+        elif type(player) == Mafia:
+            return "Mafia"
+        else:
+            return "Civilian"
 
 
 class Doctor(Player):
-    pass
+    def __init__(self, name):
+        super().__init__(name)
+
+    @staticmethod
+    def treat(player):
+        player.set_immunity()
 
 
 class NightFly(Player):
+    def __init__(self, name):
+        super().__init__(name)
+        self.night_person = None
+
+    def spend_night(self, player):
+        player.set_immunity()
+        self.night_person = player
+
+
+if __name__ == "__main__":
     pass
